ef8091b3d72eca635dd82012f24df03e143f6419
AlanProgrammer93 <alanwebprogrammer@gmail.com>
2025-04-05 13:43:02 -0300

    fix 3

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     output: {
         filename: '[name]-[contenthash:6].bundle.js',
         path: path.join(__dirname, 'build'),
-        publicPath: `/`
+        publicPath: `./`
     },
     resolve: {
         mainFields: ['browser', 'module', 'main'],
